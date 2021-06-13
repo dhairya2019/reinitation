fcb557a1d366f1a986f584735123a74c21609944
Dhairya <dhairya.chugh77@gmail.com>
2021-06-13 23:24:22 +0530

    Update and rename power.cpp to fastpower.cpp

--- recursion/fastpower.cpp
+++ recursion/fastpower.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fastpower(int a , int b){
+    if(b==0)
+    return 1;
+    int smallpower = fastpower(a,b/2);
+    int smallpower1 = smallpower*smallpower;
+    if(b & 1){
+        return a*smallpower1;
+    }
+    return smallpower1;
+    
+}
+
 int power(int a , int b){
     //Function to calculate power
     if(b==0)
     return 1;
-    if(b==1)
-    return a;
     else
     return a*power(a,b-1);
 
@@ -18,5 +28,6 @@ int main()
     int a=0,b=0;
     cin>>a>>b;
     cout<<power(a,b)<<endl;
+    cout<<fastpower(a,b)<<endl;
     return 0;
 }
