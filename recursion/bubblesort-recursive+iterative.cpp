704c48f3208ae697fa5525d5cddc983cb29a9b49
Dhairya <dhairya.chugh77@gmail.com>
2021-06-16 01:30:09 +0530

    Update bubblesort-recursive+iterative.cpp

--- recursion/bubblesort-recursive+iterative.cpp
+++ recursion/bubblesort-recursive+iterative.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+//Iterative+Recursive Approach
 void bubblesort(int *arr, int n){
    //Base case
    if(n==1){
@@ -19,12 +19,35 @@ void bubblesort(int *arr, int n){
     
 }
 
+//Recursive Approach
+void bubblesort(int * arr, int n, int j){
+    //Base case
+    if(n==1){
+        return;
+    }
+    
+    if(j==n-1){
+        return bubblesort(arr,n-1,0);
+    }
+    
+    //Recursive case
+    if(arr[j]>arr[j+1]){
+        swap(arr[j],arr[j+1]);
+    }
+    
+    bubblesort(arr,n,j+1);
+    return;
+}
+
+
 
 int main()
 {
     
   int arr[]={1,4,6,7,11,12};
   int n= sizeof(arr)/sizeof(arr[0]);
+  int j=0;
+  bubblesort(arr,n,j);
   for(int i=0;i<n;i++){
       cout<<arr[i]<<" ";
   }
