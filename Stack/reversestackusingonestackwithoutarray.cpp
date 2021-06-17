f501f312d69b21af24e4c6e2584aebc77b671d5a
Dhairya <dhairya.chugh77@gmail.com>
2021-06-17 20:59:12 +0530

    Create reversestackusingonestackwithoutarray.cpp

---
+++ Stack/reversestackusingonestackwithoutarray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<stack>
+using namespace std;
+
+void transfer(stack<int>& s1, stack<int>& s2, int n){
+    for(int i=0;i<n;++i){
+    s2.push(s1.top());
+    s1.pop();
+    }
+}
+void reverseStack(stack<int>& s1){
+    stack<int> s2;
+    int size = s1.size();
+    for(int i=0;i<size;++i){
+        int x = s1.top();
+        s1.pop();
+        transfer(s1,s2, size-i-1);
+        s1.push(x);
+        transfer(s2,s1,size-i-1);
+    }
+}
+
+int main(){
+    stack<int> s;
+    s.push(4);
+    s.push(2);
+    s.push(3);
+    s.push(1);
+    
+    reverseStack(s);
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+    
+    
+    return 0;
+}
